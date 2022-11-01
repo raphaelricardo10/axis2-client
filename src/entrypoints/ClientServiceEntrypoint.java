ecd43961a12500e615d6a2e0ec250584286bd9d7
Raphael <raphaelricardo10@gmail.com>
2022-10-31 23:00:48 -0300

    Main: used the update method from client service

--- src/entrypoints/ClientServiceEntrypoint.java
+++ src/entrypoints/ClientServiceEntrypoint.java
@@ -13,6 +13,17 @@ public class ClientServiceEntrypoint {
         PersonStub createdClient = services.clientService.createClient("Raphael Gonçalves", "raphaelricardo10@gmail.com", "161.491.137-10", "MALE",
         "(21) 98678-6884", "1996-12-27");
 
+        createdClient.setName("Lucas Ferreira");
+
+        services.clientService.updateClient(createdClient);
+
+        createdClient = services.clientService.getClients()[0];
+        
         System.out.println(createdClient.getName());
+
+        services.clientService.removeClient(createdClient.getCpf());
+
+        System.out.println(services.clientService.getClients());
+
     }
 }
